e4b29352e0ab804133d9415ef7964c96998f8dd2
vinchamp77 <vinchamp77@gmail.com>
2022-07-08 18:13:36 +1000

    Minor clean up

--- app/src/main/java/com/example/understandlifecyclesdemo/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/understandlifecyclesdemo/ui/navigation/NavGraph.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.understandlifecyclesdemo.ui.screens.HomeScreen
-import com.example.understandlifecyclesdemo.ui.screens.LoginScreen
+import com.example.understandlifecyclesdemo.ui.screens.SecondScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
@@ -26,7 +26,7 @@ private fun addLoginScreen(
     navGraphBuilder: NavGraphBuilder
 ) {
     navGraphBuilder.composable(route = NavRoute.Login.path) {
-        LoginScreen(
+        SecondScreen(
             navigateToHome = {
                 navController.navigate(NavRoute.Home.path)
             }
--- app/src/main/java/com/example/understandlifecyclesdemo/ui/screens/FirstScreen.kt
+++ app/src/main/java/com/example/understandlifecyclesdemo/ui/screens/FirstScreen.kt
@@ -1,25 +1,17 @@
 package com.example.understandlifecyclesdemo.ui.screens
 
 import android.content.Intent
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.understandlifecyclesdemo.ui.MainActivity
 import com.example.understandlifecyclesdemo.ui.MainViewModel
 import com.example.understandlifecyclesdemo.ui.MainViewModelFactory
 import com.example.understandlifecyclesdemo.ui.SecondActivity
@@ -27,7 +19,7 @@ import com.example.understandlifecyclesdemo.ui.common.DefaultButton
 import com.example.understandlifecyclesdemo.ui.theme.SimpleNavComposeAppTheme
 
 @Composable
-fun LoginScreen(
+fun SecondScreen(
     navigateToHome: () -> Unit
 ) {
     //compose navigation creates a new view model store owner for each destination
@@ -46,7 +38,7 @@ fun LoginScreen(
         )
 
         DefaultButton(
-            text = "Focus Demo",
+            text = "Loses Focus",
             onClick = {
                 context.startActivity(Intent(context, SecondActivity::class.java))
             }
@@ -62,7 +54,7 @@ private fun DefaultPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            LoginScreen(
+            SecondScreen(
                 navigateToHome = {}
             )
         }
